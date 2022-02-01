4d2b783b920f02f3f54498404beebfaa18433871
Mario Hamann <mario.hamann@virtual-identity.com>
2022-02-01 22:55:52 +0100

    first commit

---
+++ src/my-element.ts
@@ -0,0 +1,60 @@
+import { html, css, LitElement } from 'lit'
+import { customElement, property } from 'lit/decorators.js'
+
+/**
+ * An example element.
+ *
+ * @slot - This element has a slot
+ * @csspart button - The button
+ */
+@customElement('my-element')
+export class MyElement extends LitElement {
+
+  /**
+   * The name to say "Hello" to.
+   */
+  @property()
+  name = 'World'
+
+  /**
+   * The number of times the button has been clicked.
+   */
+  @property({ type: Number })
+  count = 0
+
+  render() {
+    return html`
+      <link rel="stylesheet" href="src/index.css">
+      <div class="bg-indigo-700">
+        <div class="max-w-2xl mx-auto text-center py-16 px-4 sm:py-20 sm:px-6 lg:px-8">
+          <h2 class="text-3xl font-extrabold text-white sm:text-4xl">
+            <span class="block">Hello, ${this.name}!</span>
+          </h2>
+          <p class="mt-4 text-lg leading-6 text-indigo-200">
+            Ac euismod vel sit maecenas id pellentesque eu sed consectetur. Malesuada adipiscing sagittis vel nulla nec.
+            <slot></slot>
+          </p>
+          
+          <button class="mt-8 w-full inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-indigo-600 bg-white hover:bg-indigo-50 sm:w-auto" @click=${this._onClick} part="button">
+            Click Count: ${this.count}
+          </button>
+        </div>
+      </div>
+      
+    `
+  }
+
+  private _onClick() {
+    this.count++
+  }
+
+  foo(): string {
+    return 'foo'
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'my-element': MyElement
+  }
+}
