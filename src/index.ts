abaa479a7a37ce67fc2301d92bf3dd11563cf5de
Mario Hamann <mario.hamann@virtual-identity.com>
2022-02-15 18:10:42 +0100

    docs: ✏️ add documentation

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,17 @@
-// Make Tailwind-CSS globally accessible
+/*
+* 1. Make Tailwind-CSS globally accessible
+* 
+* This approach seems to be okay facing the following sentence:
+* "Many modern browsers implement an optimization for <style> tags either cloned from a common node or that have identical text, to allow them to share a single backing stylesheet.
+* With this optimization the performance of external and internal styles should be similar." (See: https://developer.mozilla.org/en-US/docs/Web/Web_Components/Using_custom_elements#internal_vs._external_styles)
+*/
 import classes from './styles/tailwind.css'
 export const styles = classes;
 
+
+/*
+* 2. Import all components
+*/
 const modules = import.meta.glob('./components/**/!(*.stories).ts')
 
 for (const path in modules) {
