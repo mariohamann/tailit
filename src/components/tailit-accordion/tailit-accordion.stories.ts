5ae385915d83e3f11caeb066b785a7db30abd9da
Mario Hamann <mario.hamann@virtual-identity.com>
2022-02-21 14:52:46 +0100

    init: re-add accordion...

--- src/components/tailit-accordion/tailit-accordion.stories.ts
+++ src/components/tailit-accordion/tailit-accordion.stories.ts
@@ -4,19 +4,19 @@ import '../../index';
 export const Default = (args: any) =>
   html`
   <tailit-accordion class="w-full" summary="Toggle Me">
-  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
-  aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-</tailit-accordion>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+    aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+  </tailit-accordion>
 `;
 
 export default {
   title: 'Components/Accordion',
   component: "tailit-accordion",
   argTypes: {
-    slot: {
-      control: { type: 'text' },
-      defaultValue: 'Slot',
-    },
+    // slot: {
+    //   control: { type: 'text' },
+    //   defaultValue: 'Slot',
+    // },
     // color: {
     //   control: { type: 'select' },
     //   options: properties.colors,
